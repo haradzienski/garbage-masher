996b0d236aa4ec2876736d637dfed395718f87a0
Mikalai Ziuz <haradzienski@gmail.com>
2020-11-19 15:21:07 -0800

    first iteration: basic implementation

--- MashingService/MashingService.cs
+++ MashingService/MashingService.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
 namespace MashingService
 {
     public class MashingService
     {
         public string Mash(string input) {
-            throw new NotImplementedException();
+            var freqMap = BuildFreqMap(input);
+            var outputBuilder = new StringBuilder(input.Length);
+
+            foreach (KeyValuePair<char, int> entry in freqMap) {
+                if (entry.Value > 1) {
+                    outputBuilder.AppendFormat("{0}{1}", entry.Key, entry.Value);
+                } else {
+                    outputBuilder.Append(entry.Key);
+                }
+            }
+
+            return outputBuilder.ToString();
+        }
+
+        private Dictionary<char, int> BuildFreqMap(string input) {
+            var dictionary = new Dictionary<char, int>();
+            foreach (var symbol in input)
+            {
+                if (!dictionary.ContainsKey(symbol)) {
+                    dictionary.Add(symbol, 0);
+                }
+
+                dictionary[symbol]++;
+            }
+
+            return dictionary;
         }
     }
 }
