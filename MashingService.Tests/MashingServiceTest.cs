7f1221e2253aa316f213e746ca0ef495cdd2bf94
Mikalai Ziuz <haradzienski@gmail.com>
2020-11-19 15:29:53 -0800

    second test - check for symbols that occur only twice

--- MashingService.Tests/MashingServiceTest.cs
+++ MashingService.Tests/MashingServiceTest.cs
@@ -7,13 +7,22 @@ namespace MashingService.Tests
     public class MashingServiceTest
     {
         [Fact]
-        public void Mash_CompactsString_WhenDoingSoShortensIt()
+        public void Mash_ShouldCompactString_WhenDoingSoShortensIt()
         {
             var masher = new MashingService();
 
             var mashed = masher.Mash("uuueeeenzzzzz");
 
             Assert.Equal("u3e4nz5", mashed);
+        }
+
+        [Fact]
+        public void Mash_ShouldNotCompactSymbols_ThatOnlyOccurTwice() {
+            var masher = new MashingService();
+
+            var mashed = masher.Mash("uuueeeennzzzzz");
+
+            Assert.Equal("u3e4nnz5", mashed);
         }
     }
 }
